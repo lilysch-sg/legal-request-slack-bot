947186d2393c58ff966ea3d0055c4bbbcefeb32a
lilysch-sg <lily@sourcegraph.com>
2025-07-23 12:17:04 -0400

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,16 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: 'Bot is working!' });
+  console.log('Slack request received:', req.method);
+  
+  if (req.method === 'POST') {
+    const { challenge } = req.body;
+    
+    if (challenge) {
+      console.log('Challenge:', challenge);
+      return res.status(200).send(challenge);
+    }
+    
+    return res.status(200).send('OK');
+  }
+  
+  res.status(200).send('Slack endpoint working!');
 }
